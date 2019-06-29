2f6e3c22488f71dff19682af2621a47cb7d9252a
giorgi94 <giorgik1994@gmail.com>
2019-06-29 22:10:51 +0400

    making navigation html

--- src/App.svelte
+++ src/App.svelte
@@ -2,17 +2,21 @@
   let name = "world";
 </script>
 
-<style lang="stylus">
-  .toolbar {
-    padding: 10px 20px;
-  }
-  .title {
-    font-size: 25px;
-  }
-</style>
-
 <div class="toolbar layout">
   <div class="title">Files</div>
+  <div class="spacer" />
+  <div class="location">
+    <input type="text" placeholder="location..." />
+  </div>
+  <button class="btn" data-label="refresh" title="Go/Refresh" disabled>
+    <i class="fas fa-redo" />
+  </button>
+  <button class="btn" data-label="back" title="Back">
+    <i class="fas fa-chevron-left" />
+  </button>
+  <button class="btn" data-label="forward" title="Forward">
+    <i class="fas fa-chevron-right" />
+  </button>
   <div class="spacer" />
 </div>
 
