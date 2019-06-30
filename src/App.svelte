5ca1fc53348ab39dbc9a950a61f6c2725ac45ed9
giorgi94 <giorgik1994@gmail.com>
2019-06-30 22:29:26 +0400

    making top navigation buttons

--- src/App.svelte
+++ src/App.svelte
@@ -6,18 +6,24 @@
   <div class="title">Files</div>
   <div class="spacer" />
   <div class="location">
-    <input type="text" placeholder="location..." />
+    <input type="text" />
   </div>
-  <button class="btn" data-label="refresh" title="Go/Refresh" disabled>
+  <button class="btn" data-label="refresh" title="Go/Refresh">
     <i class="fas fa-redo" />
   </button>
-  <button class="btn" data-label="back" title="Back">
+  <button class="btn" data-label="back" title="Back" disabled>
     <i class="fas fa-chevron-left" />
   </button>
   <button class="btn" data-label="forward" title="Forward">
     <i class="fas fa-chevron-right" />
   </button>
   <div class="spacer" />
+  <button class="btn" data-label="list" title="List View">
+    <i class="fas fa-list" />
+  </button>
+  <button class="btn active" data-label="grid" title="Grid View">
+    <i class="fas fa-th" />
+  </button>
 </div>
 
 <hr class="divider" />
